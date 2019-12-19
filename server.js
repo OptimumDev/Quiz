648675eff220328c8f07948a2e11c126f7baf9bb
grygr <grygry0@gmail.com>
2019-12-19 10:54:47 +0500

    add questions and id

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const uuid = require('uuid/v4');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,11 +7,12 @@ app.use(express.static("static"));
 
 class QuestionData {
     constructor(image, question, answers, right) {
+        this.id = uuid();
         this.image = image;
         this.question = question;
         this.answers = answers;
         if (right >= answers.length)
-            throw "There are not enough answers"
+            throw "There are not enough answers";
         this.right = right;
     }
 }
@@ -19,14 +20,18 @@ class QuestionData {
 const questions = [
     new QuestionData('tiger', 'Какой тигр самый крупный?', ['Амурский', 'Малазийский', 'Индийский', 'Суматранский'], 1),
     new QuestionData('coala', 'Где живет коала?', ['В горной пещере', 'В бамбуковом лесу', 'В депрессии', 'В тропическом лесу'], 3)
-]
+].reduce((map, x) => {
+    map.set(x.id, x);
+    return map;
+}, new Map())
 
 
 
 
 
 app.get("/questions", (req, res) => {
-    res.send(questions)
+    console.log(questions)
+    res.send([...questions.values()])
 });
 
 
